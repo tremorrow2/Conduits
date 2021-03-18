0a0801b2fcffdbc46a68bc694efafa826f4245cc
tremorrow2 <78444166+tremorrow2@users.noreply.github.com>
2021-03-18 12:44:44 -0700

    Update Conduit.java
    Change the if else statements to Map to allow for more efficient retrieval of the data.

--- Conduit.java
+++ Conduit.java
@@ -1,140 +1,33 @@
+import java.util.*;
 public class Conduit {
     double area;
+    Map<String, Double> wires;
+    Map<Integer, Map<Integer,Double>> multicables;
+    Map<Integer, Double> fiberoptic;
+    Map<String, Double> cs;
+    Map<Integer,Map<String,Double>> trades;
 
     public Conduit() {
       area = 0.00;
+      create_wires();
+      create_fiberoptic();
+      create_multicables();
+      create_cs();
+      create_trades();
     }
 
     public void add_wires(String type, int amount) {
       double wire_area = 0;
       double total_area = 0;
-      if(type.equals("14")) {
-        wire_area = .023;
-      } else if (type.equals("12")) {
-        wire_area = .029;
-      } else if (type.equals("10")) {
-        wire_area = .035;
-      } else if (type.equals("8")) {
-        wire_area = .062;
-      } else if (type.equals("6")) {
-        wire_area = .081;
-      } else if (type.equals("4")) {
-        wire_area = .108;
-      } else if (type.equals("2")) {
-        wire_area = .146;
-      } else if (type.equals("1")) {
-        wire_area = .197;
-      } else if (type.equals("1/0")) {
-        wire_area = .229;
-      } else if (type.equals("2/0")) {
-        wire_area = .274;
-      } else if (type.equals("3/0")) {
-        wire_area = .322;
-      } else if (type.equals("4/0")) {
-        wire_area = .385;
-      } else if (type.equals("250 kcmil")) {
-        wire_area = .478;
-      } else if (type.equals("300 kcmil")) {
-        wire_area = .555;
-      } else {
-        wire_area = 0;
-        System.out.println("Wire is not within current inventory");
-      }
+      wire_area = wires.get(type);
       total_area = wire_area * amount;
       area += total_area;
     }
 
-    public void add_multiCable(int type, int amount, int gauge) {
+    public void add_multiCable(int type, int gauge, int amount) {
       double cable_area = 0;
       double total_area = 0;
-
-      if(gauge == 14) {
-        if(type == 2) {
-          cable_area = .091327;
-        } else if(type == 3 ) {
-          cable_area = .101223;
-        } else if(type == 4 ) {
-          cable_area = .120072;
-        } else if(type == 5 ) {
-          cable_area = .14522;
-        } else if(type == 6 ) {
-          cable_area = .169093;
-        } else if(type == 7 ) {
-          cable_area = .169093;
-        } else if(type == 8 ) {
-          cable_area = .201886;
-        } else if(type == 9 ) {
-          cable_area = .256072;
-        } else if(type == 10 ) {
-          cable_area = .275254;
-        } else if(type == 12 ) {
-          cable_area = .31769;
-        } else if(type == 14 ) {
-          cable_area = .350464;
-        } else if(type == 15 ) {
-          cable_area = .389256;
-        } else if(type == 16 ) {
-          cable_area = .389256;
-        } else if(type == 19 ) {
-          cable_area = .431247;
-        } else if(type == 20 ) {
-          cable_area = .476612;
-        } else if(type == 21 ) {
-          cable_area = .476612;
-        } else if(type == 25 ) {
-          cable_area = 0;
-          System.out.println("No cable info for this size and guage");
-        } else if(type == 26 ) {
-          cable_area = .672006;
-        } else {
-          System.out.println("No info on give cable type");
-        }
-      } else if (gauge == 12) {
-        if(type == 2) {
-          cable_area = .123786;
-        } else if(type == 3 ) {
-          cable_area = .138544;
-        } else if(type == 4 ) {
-          cable_area = .165468;
-        } else if(type == 5 ) {
-          cable_area = .198713;
-        } else if(type == 6 ) {
-          cable_area = 0;
-          System.out.println("No cable info for this size and guage");
-        } else if(type == 7 ) {
-          cable_area = .263298;
-        } else if(type == 8 ) {
-          cable_area = 0;
-          System.out.println("No cable info for this size and guage");
-        } else if(type == 9 ) {
-          cable_area = .359971;
-        } else if(type == 10 ) {
-          cable_area = .369605;
-        } else if(type == 12 ) {
-          cable_area = .445328;
-        } else if(type == 14 ) {
-          cable_area = 0;
-          System.out.println("No cable info for this size and guage");
-        } else if(type == 15 ) {
-          cable_area = 0;
-          System.out.println("No cable info for this size and guage");
-        } else if(type == 16 ) {
-          cable_area = .602696;
-        } else if(type == 19 ) {
-          cable_area = .677831;
-        } else if(type == 20 ) {
-          cable_area = .748151;
-        } else if(type == 21 ) {
-          cable_area = .748151;
-        } else if(type == 25 ) {
-          cable_area = .916088;
-        } else if(type == 26 ) {
-          cable_area = 0;
-          System.out.println("No cable info for this size and guage");
-        } else {
-          System.out.println("No info on give cable type");
-        }
-      }
+      cable_area = multicables.get(gauge).get(type);
       total_area = cable_area * amount;
       area += total_area;
     }
@@ -142,169 +35,209 @@ public class Conduit {
     public void add_fiberOptic(int type, int amount) {
       double optic_area = 0;
       double total_area = 0;
-      if(type % 12 == 0) {
-          if(type >= 12 && type <= 72) {
-            optic_area = .134215;
-          } else if(type >= 84 && type <= 96) {
-            optic_area = .181193;
-          } else if(type >= 108 && type <= 144) {
-            optic_area = .303904;
-          } else if(type >= 156 && type <= 216) {
-            optic_area = .311646;
-          } else if(type >= 228 && type <= 280) {
-            optic_area = .403241;
-          } else if(type == 432) {
-            optic_area = .547135;
-          } else {
-            optic_area = 0;
-            System.out.println("No info for given Fiber Optic Cable");
-          }
-      } else {
-        System.out.println("No info for given Fiber Optic Cable");
-      }
-
+      optic_area = fiberoptic.get(type);
       total_area = area * amount;
       area += total_area;
     }
 
-    public void add_circuitCable(String type, int amount) {
-      double other_area = 0;
+    public void add_cs(String type, int amount) {
+      double cs_area = 0;
       double total_area = 0;
-      if(type.equals("2cs")) {
-        other_area = .074991;
-      } else if(type.equals("3cs")) {
-        other_area = .061575;
-      }
-
-      total_area = other_area * amount;
+      cs_area = cs.get(type);
+      total_area = cs_area * amount;
       area += total_area;
 
     }
 
-    public double size() {
-      return area;
+    public void trade_size(String type, int schedule) {
+      double perc = 0.00;
+      double trade_area = 0;
+      double filled = 0;
+      double temp_filled = 0;
+      String trade = "";
+      if(type.equalsIgnoreCase("old")) {
+        perc = 0.40;
+      } else {
+        perc = 0.26;
+      }
+      Map<String, Double> temp = trades.get(schedule);
+      Set<String> temp2 = temp.keySet();
+      Iterator<String> look = temp2.iterator();
+      trade = look.next();
+      trade_area = temp.get(trade);
+      while(area > (trade_area * perc)) {
+        trade = look.next();
+        trade_area = temp.get(trade);
+      }
+      temp_filled = (area/trade_area);
+      double scale = Math.pow(10,4);
+      filled = Math.round(temp_filled * 100);
+      System.out.println("The mine size of your conduit is " + trade);
+      System.out.println("The combined area of the wires is " + area + "in squared");
+      System.out.println("The percentage filled is " + filled + "%");
+
     }
 
-    public String trade_size(String type, int schedule) {
-      String trade = "";
-      if(schedule == 40) {
-        if(type.equalsIgnoreCase("old")) {
-          if(area <= .114) {
-            trade = "1/2";
-          } else if(area <= .2032) {
-            trade = "3/4";
-          } else if(area <= .3328) {
-            trade = "1";
-          } else if(area <= .5812) {
-            trade = "1 1/4";
-          } else if(area <= .7944) {
-            trade = "1 1/2";
-          }  else if(area <= 1.3164) {
-            trade = "2";
-          }  else if(area <= 1.878) {
-            trade = "2 1/2";
-          }  else if(area <= 2.9072) {
-            trade = "3";
-          }  else if(area <= 3.8948) {
-            trade = "3 1/2";
-          }  else if(area <= 5.0216) {
-            trade = "4";
-          }  else if(area <= 7.9044) {
-            trade = "5";
-          }  else if(area <= 11.4268) {
-            trade = "6";
-          }  else {
-            trade = "There is no conduit that fits this area.";
-          }
-        } else {
-          if(area <= .0741) {
-            trade = "1/2";
-          } else if(area <= .13208) {
-            trade = "3/4";
-          } else if(area <= .21632) {
-            trade = "1";
-          } else if(area <= .37778) {
-            trade = "1 1/4";
-          } else if(area <= .51636) {
-            trade = "1 1/2";
-          }  else if(area <= .85566) {
-            trade = "2";
-          }  else if(area <= 1.2207) {
-            trade = "2 1/2";
-          }  else if(area <= 1.88968) {
-            trade = "3";
-          }  else if(area <= 2.53162) {
-            trade = "3 1/2";
-          }  else if(area <= 3.26404) {
-            trade = "4";
-          }  else if(area <= 5.13786) {
-            trade = "5";
-          }  else if(area <= 7.42742) {
-            trade = "6";
-          }  else {
-            trade = "There is no conduit that fits this area.";
-          }
-        }
-      } else {
-        if(type.equalsIgnoreCase("old")) {
-          if(area <= .0868) {
-            trade = "1/2";
-          } else if(area <= .1636) {
-            trade = "3/4";
-          } else if(area <= .2752) {
-            trade = "1";
-          } else if(area <= .4948) {
-            trade = "1 1/4";
-          } else if(area <= .6844) {
-            trade = "1 1/2";
-          }  else if(area <= 1.1496) {
-            trade = "2";
-          }  else if(area <= 1.6476) {
-            trade = "2 1/2";
-          }  else if(area <= 2.5768) {
-            trade = "3";
-          }  else if(area <= 3.4752) {
-            trade = "3 1/2";
-          }  else if(area <= 4.5032) {
-            trade = "4";
-          }  else if(area <= 7.142) {
-            trade = "5";
-          }  else if(area <= 10.2392) {
-            trade = "6";
-          }  else {
-            trade = "There is no conduit that fits this area.";
-          }
-        } else {
-          if(area <= .05642) {
-            trade = "1/2";
-          } else if(area <= .10634) {
-            trade = "3/4";
-          } else if(area <= .17888) {
-            trade = "1";
-          } else if(area <= .32162) {
-            trade = "1 1/4";
-          } else if(area <= .44486) {
-            trade = "1 1/2";
-          }  else if(area <= .74724) {
-            trade = "2";
-          }  else if(area <= 1.07094) {
-            trade = "2 1/2";
-          }  else if(area <= 1.67492) {
-            trade = "3";
-          }  else if(area <= 2.25888) {
-            trade = "3 1/2";
-          }  else if(area <= 2.92708) {
-            trade = "4";
-          }  else if(area <= 4.6423) {
-            trade = "5";
-          }  else if(area <= 6.65548) {
-            trade = "6";
-          }  else {
-            trade = "There is no conduit that fits this area.";
-          }
-        }
-      }
-      return trade;
+    public void create_wires() {
+      wires = new TreeMap<String,Double>();
+      wires.put("14",.023);
+      wires.put("12",.029);
+      wires.put("10",.035);
+      wires.put("8",.062);
+      wires.put("6",.081);
+      wires.put("4",.108);
+      wires.put("2",.146);
+      wires.put("1",.197);
+      wires.put("1/0",.229);
+      wires.put("2/0",.274);
+      wires.put("3/0",.322);
+      wires.put("4/0",.385);
+      wires.put("250 kcmil",.478);
+      wires.put("300 kcmil",.555);
+    }
+
+    public void create_multicables() {
+      Map<Integer, Double> multicables12 = new TreeMap<Integer,Double>();
+      Map<Integer, Double> multicables14 = new TreeMap<Integer,Double>();
+      multicables = new TreeMap<Integer,Map<Integer,Double>>();
+      multicables12.put(2,.123786);
+      multicables12.put(3,.138544);
+      multicables12.put(4,.165468);
+      multicables12.put(6,0.0);
+      multicables12.put(5,.198713);
+      multicables12.put(7,.263298);
+      multicables12.put(8,0.0);
+      multicables12.put(9,.359971);
+      multicables12.put(10,.369605);
+      multicables12.put(12,.445328);
+      multicables12.put(14,0.0);
+      multicables12.put(15,0.0);
+      multicables12.put(16,.602696);
+      multicables12.put(19,.677831);
+      multicables12.put(20,.748151);
+      multicables12.put(21,.748151);
+      multicables12.put(25,.916088);
+      multicables12.put(26,0.0);
+      multicables14.put(2,.091327);
+      multicables14.put(3,.101223);
+      multicables14.put(4,.120072);
+      multicables14.put(5,.14522);
+      multicables14.put(6,.169093);
+      multicables14.put(7,.169093);
+      multicables14.put(8,.201886);
+      multicables14.put(9,.256072);
+      multicables14.put(10,.275254);
+      multicables14.put(12,.31769);
+      multicables14.put(14,.350464);
+      multicables14.put(15,.389256);
+      multicables14.put(16,.389256);
+      multicables14.put(19,.431247);
+      multicables14.put(20,.476612);
+      multicables14.put(21,.476612);
+      multicables14.put(25,0.0);
+      multicables14.put(26,.672006);
+      multicables.put(12,multicables12);
+      multicables.put(14,multicables14);
+    }
+
+    public void create_trades() {
+
+      Map<String, Double> TradeArea40 = new TreeMap<String, Double>();
+      Map<String, Double> TradeArea80 = new TreeMap<String, Double>();
+      trades = new TreeMap<Integer,Map<String, Double>>();
+      TradeArea40.put("1/2",.285);
+      TradeArea40.put("3/4",.508);
+      TradeArea40.put("1",.832);
+      TradeArea40.put("1 1/4",1.453);
+      TradeArea40.put("1 1/2",1.986);
+      TradeArea40.put("2",3.291);
+      TradeArea40.put("2 1/2",4.695);
+      TradeArea40.put("3",7.268);
+      TradeArea40.put("3 1/2",9.737);
+      TradeArea40.put("4",12.554);
+      TradeArea40.put("5",19.761);
+      TradeArea40.put("6",28.567);
+
+      TradeArea80.put("1/2",.217);
+      TradeArea80.put("3/4",.409);
+      TradeArea80.put("1",.688);
+      TradeArea80.put("1 1/4",1.237);
+      TradeArea80.put("1 1/2",1.711);
+      TradeArea80.put("2",2.874);
+      TradeArea80.put("2 1/2",4.119);
+      TradeArea80.put("3",6.442);
+      TradeArea80.put("3 1/2",8.688);
+      TradeArea80.put("4",11.258);
+      TradeArea80.put("5",17.855);
+      TradeArea80.put("6",25.598);
+
+      trades.put(40,TradeArea40);
+      trades.put(80,TradeArea80);
+
+
+    }
+    public void create_fiberoptic(){
+      fiberoptic = new TreeMap<Integer,Double>();
+      fiberoptic.put(12,.134215);
+      fiberoptic.put(24,.134215);
+      fiberoptic.put(36,.134215);
+      fiberoptic.put(48,.134215);
+      fiberoptic.put(60,.134215);
+      fiberoptic.put(72,.134215);
+      fiberoptic.put(84,.181193);
+      fiberoptic.put(96,.181193);
+      fiberoptic.put(108,.303904);
+      fiberoptic.put(120,.303904);
+      fiberoptic.put(144,.303904);
+      fiberoptic.put(156,.311646);
+      fiberoptic.put(192,.311646);
+      fiberoptic.put(216,.311646);
+      fiberoptic.put(228,.403241);
+      fiberoptic.put(240,.403241);
+      fiberoptic.put(288,.403241);
+      fiberoptic.put(432,.547135);
+    }
+
+    public void create_cs(){
+      cs = new TreeMap<String, Double>();
+      cs.put("2cs",.074991);
+      cs.put("3cs",.061575);
+    }
+
+    public String print_wires(){
+      Set<String> wires2 = wires.keySet();
+      return wires2.toString();
+    }
+
+    public String print_multicables(){
+      Set<Integer> multicables2 = multicables.get(14).keySet();
+      return multicables2.toString();
+    }
+
+    public String print_fiberoptics(){
+      Set<Integer> fiberoptic2 = fiberoptic.keySet();
+      return fiberoptic2.toString();
+    }
+
+    public String print_cs(){
+      Set<String> cs2 = cs.keySet();
+      return cs2.toString();
+    }
+
+    public boolean contain_wire(String s){
+      return wires.containsKey(s);
+    }
+
+    public boolean contain_multicables(int gauge, int x){
+      return multicables.get(gauge).containsKey(x);
+    }
+
+    public boolean contain_fiberoptic(int x){
+      return fiberoptic.containsKey(x);
+    }
+
+    public boolean contain_cs(String s){
+      return cs.containsKey(s);
     }
 
 }
